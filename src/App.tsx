994d5adb4a738972651325d30d7ae3fa0b1ac97c
Szepesi Tibor <sztibi023@gmail.com>
2023-03-09 04:42:48 +0100

    feat: init services

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import { Route, Router, Routes } from '@solidjs/router'
 import LandingPage from './pages/LandingPage'
 import LoginPage from './pages/Login'
 import RegisterPage from './pages/Register'
-import TeamPage, { CreateTeam, JoinTeam, ManageTeam } from './pages/Teams'
+import { CreateTeam, JoinTeam, ManageTeam } from './pages/Teams'
 import WaitPage from './pages/Wait'
+import { SocketServiceSingleton } from './services/socket'
+import { AuthService } from './services/auth'
+
+export const authService = new AuthService('http://localhost:3001')
+export const socketService = new SocketServiceSingleton('http://localhost:3002')
 
 const App: Component = () => {
   return (
