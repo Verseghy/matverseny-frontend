77ca7db19dfbdc46f4a8bce76a4455aa9b6d9eb9
Szepesi Tibor <sztibi023@gmail.com>
2023-03-10 00:16:40 +0100

    refactor: move timer to a separate component

---
+++ src/components/Timer.tsx
@@ -0,0 +1,39 @@
+import { Component, createRenderEffect, createSignal, from } from 'solid-js'
+
+export type TimerProps = {
+  time: Date,
+}
+
+const Timer: Component<TimerProps> = (props) => {
+  const [formattedTime, setFormattedTime] = createSignal('00:00:00')
+
+  const clock = from((set) => {
+    const t = setInterval(() => set(1), 1000);
+    return () => clearInterval(t);
+  });
+
+  createRenderEffect(() => {
+    clock()
+
+    const diff = props.time.getTime() - new Date().getTime()
+    if (diff < 0) return
+
+    setFormattedTime(formatTime(diff))
+  })
+
+  return <>{formattedTime()}</>
+}
+
+export default Timer
+
+const formatTime = (time: number) => {
+  const hours = Math.floor(time / 3600000)
+  const minutes = Math.floor(time / 60000) % 60
+  const seconds = Math.floor(time / 1000) % 60
+
+  const hoursString = hours < 10 ? `0${hours}` : hours
+  const minutesString = `0${minutes}`.slice(-2)
+  const secondsString = `0${seconds}`.slice(-2)
+
+  return `${hoursString}:${minutesString}:${secondsString}`
+}
--- src/pages/Wait.tsx
+++ src/pages/Wait.tsx
@@ -1,41 +1,20 @@
-import { Component, createEffect, createSignal, from } from 'solid-js'
+import { Component } from 'solid-js'
 import Button from '../components/Button'
 import Card from '../components/Card'
+import Timer from '../components/Timer'
 import styles from './Wait.module.scss'
 
-const formatTime = (time: number) => {
-  const hours = Math.floor(time / 3600000)
-  const minutes = Math.floor(time / 60000) % 60
-  const seconds = Math.floor(time / 1000) % 60
-
-  const hoursString = hours < 10 ? `0${hours}` : hours
-  const minutesString = `0${minutes}`.slice(-2)
-  const secondsString = `0${seconds}`.slice(-2)
-
-  return `${hoursString}:${minutesString}:${secondsString}`
-}
-
 const WaitPage: Component = () => {
-  const [time, setTime] = createSignal(new Date(new Date().getTime() + 1*60000))
-  const [formattedTime, setFormattedTime] = createSignal('00:00:00')
-
-  const clock = from((set) => {
-    const t = setInterval(() => set(1), 1000);
-    return () => clearInterval(t);
-  });
-
-  createEffect(() => {
-    clock()
-    const diff = Math.abs(time().getTime() - new Date().getTime())
-    setFormattedTime(formatTime(diff))
-  })
+  const time = new Date(new Date().getTime() + 1*60000)
 
   return (
     <div class={styles.container}>
       <Card>
         <h1>Kérlek várj!</h1>
         <p>A verseny kezdetéig még:</p>
-        <p class={styles.time}>{formattedTime()}</p>
+        <p class={styles.time}>
+          <Timer time={time} />
+        </p>
         <Button href="/team">Csapathoz</Button>
       </Card>
     </div>
@@ -43,4 +22,3 @@ const WaitPage: Component = () => {
 }
 
 export default WaitPage
-
