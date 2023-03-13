7cb7164d014ecf7145eafd291c04a8cdd98650a5
Zoltán Szepesi <2dcube01@gmail.com>
2023-03-13 17:25:57 +0100

    Add solution service

---
+++ src/services/solution.ts
@@ -0,0 +1,26 @@
+import {Observable, of, switchMap} from "rxjs";
+import {fromFetch} from "rxjs/internal/observable/dom/fetch";
+import {baseFetchRequest} from "./fetch";
+
+export class SolutionService {
+    constructor(private baseURLMathCompetition: string) {
+    }
+
+    setSolution({problem, solution}: {problem: string, solution: number | null}): Observable<void> {
+        return fromFetch(`${this.baseURLMathCompetition}/team/create`, {
+            ...baseFetchRequest(),
+            method: "POST",
+            body: JSON.stringify({
+                problem,
+                solution
+            })
+        }).pipe(
+            switchMap(async resp => {
+                if (!resp.ok) {
+                    const r = await resp.json()
+                    throw {code: r.code}
+                }
+            })
+        )
+    }
+}
