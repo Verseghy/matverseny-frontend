430f0425844108241bb77b48410ccd44fbf386a5
Szepesi Tibor <sztibi023@gmail.com>
2023-03-13 18:02:07 +0100

    Use solution instead of answer

--- src/pages/Competition.tsx
+++ src/pages/Competition.tsx
@@ -1,5 +1,7 @@
+import { useNavigate } from '@solidjs/router'
 import { FaSolidChevronLeft, FaSolidChevronRight } from 'solid-icons/fa'
-import { Component, createEffect, For } from 'solid-js'
+import { Component, createEffect, For, from } from 'solid-js'
+import { jwtService, socketService } from '../App'
 import Button from '../components/Button'
 import Paginator from '../components/Paginator'
 import { ProblemCard } from '../components/ProblemCard'
@@ -17,7 +19,7 @@ const QuickProblemButton: Component<QuickProblemButtonProps> = (props) => {
   return (
     <Button
       class={styles.problemButton}
-      kind={props.problem.answer ? 'primary' : undefined}
+      kind={props.problem.solution ? 'primary' : undefined}
       onClick={() => {
         window.scrollTo({
           top: document.getElementById(`card_${props.problem.id}`)!.offsetTop - 24,
@@ -64,61 +66,61 @@ const CompetitionPage: Component = () => {
       id: '1',
       body: '1 some $$random$$ body $asdf$ dsa',
       image: '',
-      answer: null,
+      solution: null,
     },
     {
       id: '2',
       body: '$\\frac{1}{\\Bigl(\\sqrt{\\phi \\sqrt{5}}-\\phi\\Bigr) e^{\\frac25 \\pi}} \\equiv 1+\\frac{e^{-2\\pi}} {1+\\frac{e^{-4\\pi}} {1+\\frac{e^{-6\\pi}} {1+\\frac{e^{-8\\pi}} {1+\\cdots} } } }$',
       image: '',
-      answer: null,
+      solution: null,
     },
     {
       id: '3',
       body: '$\\mathcal L_{\\mathcal T}(\\vec{\\lambda}) = \\sum_{(\\mathbf{x},\\mathbf{s})\\in \\mathcal T} \\log P(\\mathbf{s}\\mid\\mathbf{x}) - \\sum_{i=1}^m \\frac{\\lambda_i^2}{2\\sigma^2}$',
       image: '',
-      answer: 123,
+      solution: 123,
     },
     {
       id: '4',
       body: '4 some random body $asdf$ dsa',
       image: '',
-      answer: null,
+      solution: null,
     },
     {
       id: '5',
       body: '5 some random body $asdf$ dsa',
       image: '',
-      answer: null,
+      solution: null,
     },
     {
       id: '6',
       body: '6 some random body $asdf$ dsa',
       image: '',
-      answer: null,
+      solution: null,
     },
     {
       id: '7',
       body: '7 some random body $asdf$ dsa',
       image: '',
-      answer: null,
+      solution: null,
     },
     {
       id: '8',
       body: '8 some random body $asdf$ dsa',
       image: '',
-      answer: null,
+      solution: null,
     },
     {
       id: '9',
       body: '9 some random body $asdf$ dsa',
       image: '',
-      answer: null,
+      solution: null,
     },
     {
       id: '',
       body: 'some random body $asdf$ dsa',
       image: '',
-      answer: null,
+      solution: null,
     },
   ]
 
